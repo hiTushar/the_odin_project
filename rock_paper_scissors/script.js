6844ad061c84a4b42523138a3fc8e4680cb302f4
hiTushar <tusharjain2201@gmail.com>
2021-03-13 17:00:18 +0530

    new changes

--- rock_paper_scissors/script.js
+++ rock_paper_scissors/script.js
@@ -4,18 +4,18 @@ function computerPlay(){
     return choice[Math.floor(Math.random() * 3)]
 }
 
-const outcomes = {
+const outcomes = { // minimal possible outcomes
     'paper rock'    : 'paper',
     'paper scissors': 'scissors',
     'rock scissors' : 'rock'
 }
 
 function playRound(player, computer){
-    let trial = [player, computer].sort().join(' '); 
+    let trial = [player, computer].sort().join(' '); // sorting reduces possible permutations
     let result = outcomes[trial];
     
     if(result){
-        if(player === result){
+        if(player === result){ // if it was the player who chose the winning result
             // return `${player} beats ${computer}. You Win!!`;
             return 'You';
         }
@@ -24,7 +24,7 @@ function playRound(player, computer){
             return 'Computer';
         }
     }
-    else{
+    else{ // undefined is returned in case of a draw
         return 'Draw'; 
     }
 }
---
+++ rock_paper_scissors_2.0/script.js
@@ -0,0 +1,65 @@
+const choice = ['paper', 'rock', 'scissors'];
+
+const outcomes = { // minimal possible outcomes
+    'paper rock'    : 'paper',
+    'paper scissors': 'scissors',
+    'rock scissors' : 'rock'
+}
+
+let playerSelection; 
+let pointsTable = {'you': 0, 'computer': 0, 'draw': 0};
+
+
+function select(event){
+    game(event.target.id); 
+}
+
+function game(playerSelection){
+    let computerSelection = computerPlay(); 
+    let winner = playRound ( playerSelection, computerSelection );
+    
+    pointsTable[winner]++;
+    displayResults(pointsTable); 
+
+    if(winner !== 'draw'){
+        if(pointsTable[winner] === 5){
+            displayWinner(winner); 
+            resetOption(); 
+        }
+    }
+    // console.log(pointsTable);
+}
+
+function computerPlay(){
+    return choice[Math.floor(Math.random() * 3)]
+}
+
+function playRound(player, computer){
+    let trial = [player, computer].sort().join(' '); // sorting reduces possible permutations
+    let result = outcomes[trial];
+    
+    if(result){
+        if(player === result){ // if it was the player who chose the winning result
+            return 'you';
+        }
+        else{
+            return 'computer';
+        }
+    }
+    else{ // undefined is returned in case of a draw
+        return 'draw'; 
+    }
+}
+
+
+function displayResults(points){
+    document.getElementById("results").innerHTML = `<p>You: ${points.you} | Computer: ${points.computer} | Draw: ${points.draw}</p>`
+}
+
+function displayWinner(winner){
+    document.getElementById("arena").innerHTML = `<span>${winner.charAt(0).toUpperCase() + winner.slice(1)} won!!</span>`
+}
+
+function resetOption(){
+    document.getElementById('results').innerHTML += '<button onclick="location.reload()">Reset</button>'
+}
